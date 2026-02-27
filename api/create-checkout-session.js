64697eaf3a2f799fbbb86ecd66c852d55223cb65
napierjimw-droid <napierjimw@gmail.com>
2026-02-27 15:23:29 -0500

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
 
   try {
     const { email, userId } = req.body;
-
+    console.log("Incoming userId:", userId);
     if (!email || !userId) {
       return res.status(400).json({ error: "Missing email or userId" });
     }
